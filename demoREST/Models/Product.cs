522647a1c07baaa14e07cd3aaf2a9ca0fc4ff61d
onudeep <anudeepg@nrifintech.com>
2017-09-23 17:02:45 +0530

    first commit

---
+++ demoREST/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace demoREST.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProductCatalogContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
